84918b7baeb082a8860f418a0d1e945c26296015
yangxiangyang <1161108412@qq.com>
2019-08-14 18:24:10 +0800

    jdk测试提交

---
+++ spring-jdk/src/test/java/com/yang/TestJDK.java
@@ -0,0 +1,14 @@
+package com.yang;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestJDK {
+
+    public static void main(String[] args) {
+
+        Map map=new HashMap();
+//        Hashtable
+
+    }
+}
